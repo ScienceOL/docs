f1a868c3a4d106f45ddb02616b828dd05b9bec17
Harvey Que <103566763+Mile-Away@users.noreply.github.com>
2025-05-20 09:17:21 +0800

    feat: update documentation structure and navigation for Protium, remo… (#2)
    * feat: update documentation structure and navigation for Protium, remove outdated tutorial files
    
    * Refactor code structure for improved readability and maintainability
    
    * fix: update section IDs to ensure they are correctly associated with titles

--- src/app/(cn)/development/Navi.tsx
+++ src/app/(cn)/development/Navi.tsx
@@ -5,4 +5,8 @@ export const navigation: Array<NavGroup> = [
     title: 'Development',
     links: [{ title: 'Introduction', href: `/development` }],
   },
+  {
+    title: 'Dev Docs',
+    links: [{ title: 'How to write dev docs', href: `/development/dev-docs` }],
+  },
 ]
