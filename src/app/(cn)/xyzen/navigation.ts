1828e670ef0af91f1d8edcdbd6d255f388cf14e9
SJRnhqh <sjrnhqh@outlook.com>
2025-07-27 20:05:40 +0800

    xyzen/docs structure

--- src/app/(cn)/xyzen/navigation.ts
+++ src/app/(cn)/xyzen/navigation.ts
@@ -5,7 +5,9 @@ const navigation: Array<NavGroup> = [
     title: 'Xyzen 玄藏',
     links: [
       { title: '介绍', href: `/xyzen` },
-      { title: '安装', href: `/xyzen/installation` },
+      { title: '用户指南', href: `/xyzen/guidance` },
+      { title: '部署与使用', href: `/xyzen/deploy` },
+      { title: '贡献与开发', href: `/xyzen/dev` },
     ],
   },
 ]
